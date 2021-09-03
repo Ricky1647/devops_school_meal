e392ac47ef61edd8a4415f9dcec05da9f5606b9c
Ricky1647 <64536589+Ricky1647@users.noreply.github.com>
2021-09-03 16:49:04 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,20 +4,35 @@ import os
 from flask import send_from_directory
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
-def predict():
-    # initialize the data dictionary that will be returned from the
-    # view
-    data = {'success': False}
-    print('request') 
-    # ensure an image was properly uploaded to our endpoint
-    if request.method == "GET":
-        return '''<!doctype html>
-        <link rel="icon" href="data:,">
+def main():
+        return  render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0',debug=True)
+
+
+
+
+
+
+
+"""如果不用render_template跑得出來
+def main():
+        return '''
+        <!doctype html>
+        <head>
+            <title>Upload new File</title>
+        </head>
         <title>Upload new File</title>
         <h1>Upload new File</h1>
         <form method=post enctype=multipart/form-data>
         <p><input type=file name=image>
         <input type=submit value=Upload>
-        </form>'''
-if __name__ == '__main__':
-    app.run()
+        </form>
+        '''
+"""
+
+
+
